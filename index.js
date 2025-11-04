03d4037f60b4252d03bdab2f50566e35ae442bc1
samir <sa.samir213002019@gmail.com>
2025-11-05 05:40:33 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+let ch=prompt("enter a charcter");
+
+if ((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+
+
+
+
+    {
+
+
+document.write("it is alphabet")
+
+
+
+
+    }
+
+
+
+    else if ((ch>='0' && ch<='9')  )
+
+    {
+
+
+document.write("is number")
+
+
+
+    }
+
+    else {
+    document.write("It is a Special Character");
+}
